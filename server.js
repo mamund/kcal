d1f791b37dba00b01da02248723398de676013fd
Mike Amundsen <mamund@yahoo.com>
2012-06-23 11:31:07 -0400

    update server.js

--- server.js
+++ server.js
@@ -30,9 +30,87 @@ collection.links = [
 ];
 
 function handler(req, res) {
-	res.writeHead(200,'OK',ctype);
-	res.end(JSON.stringify(collection));
+  // check URI
+	var url = req.url;
+	switch(url) {
+		case '/':
+			showRoot(req, res);
+			break;
+		case '/maintain/':
+			showMaintain(req, res);
+			break;
+		case '/expend/':
+		  showExpend(req, res);
+			break;
+		case '/deficit/':
+		  showDeficit(req, res);
+			break;
+		default:
+			showError(req, res, 404, null, 'Invalid URL');
+			break;
+	}
+}
+
+function showRoot(req, res) {
+	if(req.method !== 'GET') {
+		showError(req, res, 415, null, req.method + ' not allowed here');
+	}
+	else {
+		res.writeHead(200,'OK',ctype);
+		res.end(JSON.stringify(collection));	
+	}
+}
+
+function showMaintain(req, res) {
+	if(req.method !== 'GET') {
+		showError(req, res, 415, null, req.method + ' not allowed here');
+	}
+	else {
+		res.writeHead(200,'OK',ctype);
+		res.end(JSON.stringify(collection));	
+	}
+}
+
+function showExpend(req, res) {
+	if(req.method !== 'GET') {
+		showError(req, res, 415, null, req.method + ' not allowed here');
+	}
+	else {
+		res.writeHead(200,'OK',ctype);
+		res.end(JSON.stringify(collection));	
+	}
+}
+
+function showDeficit(req, res) {
+	if(req.method !== 'GET') {
+		showError(req, res, 415, null, req.method + ' not allowed here');
+	}
+	else {
+		res.writeHead(200,'OK',ctype);
+		res.end(JSON.stringify(collection));	
+	}
+}
+
+function showError(req, res, status, code, message) {
+	var coll = getEmptyCollection(req);
+  coll.error = {};
+	if(code) {
+		coll.error.code = code;
+	}
+	if(message) {
+		coll.error.message = message;
+	}
+	res.writeHead(status,ctype);
+	res.end(JSON.stringify(coll));
+}
+
+function getEmptyCollection(req) {
+	var c = {};
+	c.href = host + '/';
+	c.version = '1.0';
+	return c;
 }
 
 http.createServer(handler).listen(port);
 console.log('running on ' + port);
+
